60c015b9103d7f0fef70e60840c7f3cfd6abcc26
Akbar Sharofiddinov <akbarsharofiddinov@gmail.com>
2024-03-29 18:33:35 +0500

    a

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const FeedbackModal = () => {
-  return (
-    <div>FeedbackModal</div>
-  )
-}
+const FeedbackModal: React.FC = () => {
+  return <div>FeedbackModal</div>;
+};
 
-export default FeedbackModal
+export default FeedbackModal;

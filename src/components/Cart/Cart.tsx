bb8a7f3342f63c3bb1e1dc33c23dc95852232539
Akbar Sharofiddinov <akbarsharofiddinov@gmail.com>
2024-04-30 14:45:39 +0500

    Cart textarea required removed

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -187,7 +187,6 @@ const Cart: React.FC<CartProps> = ({ isOpen }: CartProps) => {
                     rows={5}
                     autoComplete="off"
                     value={comment}
-                    required
                     onChange={(e) => {
                       setComment(e.target.value);
                     }}
